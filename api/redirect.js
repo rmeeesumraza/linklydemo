fe0cf55b20475b0b7a7ba0a0d9b9309601788be8
rmeeesumraza <github@snobay.com>
2024-12-20 21:23:50 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://ramblinwreckstore.com/products/gtyjlt0065-ladies-georgia-tech-yellow-jackets-london-t-shirt";
+    const whitePageURL = "https://palmkissed.com/products/london-hoodie";
     const blackPageURL = "https://saizjdkeo.myfunnelish.com/bear-hood";
   
     // Parse the UTM parameters from the request URL
