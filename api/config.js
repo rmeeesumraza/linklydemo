05aaccd064071fe2c4322204a2e5caa90623363b
rmeeesumraza <github@snobay.com>
2025-01-15 14:52:58 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,26 +1,3 @@
-//Set your blackhat landing page
-const bhLP = `
-<html>
-    <body>
-        <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
-    </body>
-</html>
-`;
-
-//Set your whitehat landing page
-const whLP = `
-<html>
-    <body>
-        <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://www.blundstone.com/rustic-brown-premium-leather-chelsea-boots-womens-style-585"></iframe>
-    </body>
-</html>
-`;
-
-//Turn blackhat lander on or off - true = Black Hat - False = White Hat
-const isRedirectEnabled = false; 
-
-if (isRedirectEnabled == false) {
-  bhLP = whLP;
-}
-
-export const default bhLP;
+// This file exports a boolean value to control redirection logic
+const isRedirectEnabled = true; // Change this value as needed
+export default isRedirectEnabled;
