865294779eca0b779c5ef63ef58d454ee9f23f84
rmeeesumraza <github@snobay.com>
2025-01-15 15:24:33 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -3,11 +3,34 @@ import isRedirectEnabled from './config';
 export default function handler(req, res) {
 
     var bhLP = `
-    <html>
-        <body>
-            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://jzudipwaq.myfunnelish.com/tt-shp-rw"></iframe>
-        </body>
-    </html>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fullscreen iFrame</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden; /* Prevent scrolling */
+        }
+
+        iframe {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://jzudipwaq.myfunnelish.com/tt-shp-rw"></iframe>
+</body>
+</html>
     `;
     
     var whLP = `
