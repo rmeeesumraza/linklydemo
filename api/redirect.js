4001cf515fc131c9f3c30df2aa19f51215189bdb
rmeeesumraza <github@snobay.com>
2025-01-15 14:06:44 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -7,34 +7,11 @@ export default function handler(req, res) {
     const iframeSrc = isRedirectEnabled ? defaultIframeURL : alternateIframeURL;
 
     const lvh = `
-    <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Live View</title>
-            <style>
-                body {
-                    margin: 0;
-                    padding: 0;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    overflow: hidden;
-                }
-                iframe {
-                    border: none;
-                }
-            </style>
-        </head>
+    <html>
         <body>
-            <div id="live-view">
-                <iframe style="display: block; position: absolute; width: 100%; height: 100%;" 
-                        width="100%" height="100%" frameborder="0" src="${iframeSrc}"></iframe>
-            </div>
+            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="${iframeSrc}"></iframe>
         </body>
-        </html>
+    </html>
 `;
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
