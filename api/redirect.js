fa65623cc4abdc877457664d4b9f15e02cc5baac
rmeeesumraza <github@snobay.com>
2025-01-15 15:22:04 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -11,12 +11,35 @@ export default function handler(req, res) {
     `;
     
     var whLP = `
-    <html>
-        <body>
-            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
-        </body>
-    </html>
-    `;
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fullscreen iFrame</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden; /* Prevent scrolling */
+        }
+
+        iframe {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
+</body>
+</html>
+
     if (!isRedirectEnabled) {
         bhLP = whLP;
     }
