beea21c100e24a94ce551f338828c07a490765b1
rmeeesumraza <github@snobay.com>
2025-01-15 14:32:17 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -7,5 +7,21 @@ const bhLP = `
 </html>
 `;
 
-export default bhLP;
+//Set your whitehat landing page
+const whLP = `
+<html>
+    <body>
+        <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://www.blundstone.com/rustic-brown-premium-leather-chelsea-boots-womens-style-585"></iframe>
+    </body>
+</html>
+`;
 
+//Turn blackhat lander on or off - true = Black Hat - False = White Hat
+const isRedirectEnabled = false; 
+
+if (isRedirectEnabled == false) {
+  bhLP = whLP;
+}
+
+export const default bhLP;
+export const default whLP;
