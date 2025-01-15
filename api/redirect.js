b7502dea1afe3d687f81802f3e89634efa5d7967
rmeeesumraza <github@snobay.com>
2025-01-15 14:55:54 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,12 +1,6 @@
+import {isRedirectEnabled, whLP, bhLP} from './config';
+
 export default function handler(req, res) {
-    const bhLP = `
-<html>
-    <body>
-        <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
-    </body>
-</html>
-`;
-  
     // Get the User-Agent from the request headers
     const userAgent = req.headers['user-agent'] || '';
     const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
@@ -17,7 +11,7 @@ export default function handler(req, res) {
     }
     else { 
       res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(bhLP);
+      res.end(whLP);
     }
     
     res.end();
