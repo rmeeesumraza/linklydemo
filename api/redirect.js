19496d99cb245aeadbb541c43962f4a025206726
rmeeesumraza <github@snobay.com>
2025-01-15 14:16:03 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -13,28 +13,19 @@ export default function handler(req, res) {
         </body>
     </html>
 `;
-    // Parse the UTM parameters from the request URL
-    const queryParams = new URLSearchParams(req.url.split('?')[1]);
-    const utmCampaign = queryParams.get('utm_campaign');
   
     // Get the User-Agent from the request headers
     const userAgent = req.headers['user-agent'] || '';
     const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-    
-    // Redirection logic
-    if (utmCampaign === '__AID_NAME__') {
-      // UTM campaign 'l1' takes priority for both desktop and mobile
-      res.writeHead(302, { Location: whitePageURL });
-    } else if (isMobileDevice) {
-      // Mobile devices without 'l1' campaign
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(lvh);
-    } else {
-      // Desktop devices without 'l1' campaign
+
+    if (isMobileDevice) {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(lvh);
-      //res.writeHead(302, { Location: whitePageURL });
     }
-  
+    else { 
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(lvh);
+    }
+    
     res.end();
   }
