dd7fde89503eb0cfeea11279a89e579996df7bf5
rmeeesumraza <github@snobay.com>
2025-01-15 15:28:57 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,3 +1,3 @@
 // This file exports a boolean value to control redirection logic
-const isRedirectEnabled = true; // Change this value as needed
+const isRedirectEnabled = false; // Change this value as needed
 export default isRedirectEnabled;
