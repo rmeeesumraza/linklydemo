28cf34155ba66904f0d18b124e0cd2e225fac668
rmeeesumraza <github@snobay.com>
2025-01-15 13:51:29 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -4,8 +4,25 @@ export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://corkcicle.com/collections/coffee-mug/products/star-wars-coffee-mug?variant=40141759152216";
     const blackPageURL = "https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939";
-    const testUrl = "https://google.com";
-  
+    const iframeSrc = isRedirectEnabled ? defaultIframeURL : alternateIframeURL;
+
+    const liveViewHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Live View</title>
+        </head>
+        <body>
+            <div id="live-view">
+                <iframe style="display: block; position: absolute; width: 100%; height: 100%;" 
+                        width="100%" height="100%" frameborder="0" src="${iframeSrc}"></iframe>
+            </div>
+        </body>
+        </html>
+    `;
+    
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
     const utmCampaign = queryParams.get('utm_campaign');
@@ -14,21 +31,20 @@ export default function handler(req, res) {
     const userAgent = req.headers['user-agent'] || '';
     const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
-    if (isRedirectEnabled) {
-        res.writeHead(302, { Location: testUrl });
-    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(liveViewHTML);
     
-    // Redirection logic
-    if (utmCampaign === '__AID_NAME__') {
-      // UTM campaign 'l1' takes priority for both desktop and mobile
-      res.writeHead(302, { Location: whitePageURL });
-    } else if (isMobileDevice) {
-      // Mobile devices without 'l1' campaign
-      res.writeHead(302, { Location: blackPageURL });
-    } else {
-      // Desktop devices without 'l1' campaign
-      res.writeHead(302, { Location: whitePageURL });
-    }
+    // // Redirection logic
+    // if (utmCampaign === '__AID_NAME__') {
+    //   // UTM campaign 'l1' takes priority for both desktop and mobile
+    //   res.writeHead(302, { Location: whitePageURL });
+    // } else if (isMobileDevice) {
+    //   // Mobile devices without 'l1' campaign
+    //   res.writeHead(302, { Location: blackPageURL });
+    // } else {
+    //   // Desktop devices without 'l1' campaign
+    //   res.writeHead(302, { Location: whitePageURL });
+    // }
   
-    res.end();
+    // res.end();
   }
