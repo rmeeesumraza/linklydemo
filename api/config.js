e90a97f71e3a5e563ab0a347b04bee0374c0acda
rmeeesumraza <github@snobay.com>
2025-01-15 15:12:33 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,19 +1,3 @@
-const pSettings = {
-    isRedirectEnabled: true,
-    bhLP: `
-    <html>
-        <body>
-            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://xjsdiaoqpwepa.myfunnelish.com/imbassd-1736879310456939"></iframe>
-        </body>
-    </html>
-`,
-    whLP: `
-    <html>
-        <body>
-            <iframe style="display: block; position: absolute; width: 100%; height: 100%;" width="100%" height="100%" frameborder="0" src="https://www.blundstone.com/rustic-brown-premium-leather-chelsea-boots-womens-style-585"></iframe>
-        </body>
-    </html>
-`
-}
-    
-export default pSettings;
+// This file exports a boolean value to control redirection logic
+const isRedirectEnabled = true; // Change this value as needed
+export default isRedirectEnabled;
